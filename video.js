895321b525f429d2c669bb8dc893be013445183a
Derek Nelson <dereknelson01@gmail.com>
2011-08-15 14:00:53 -0400

    Debugging fixes.

--- video.js
+++ video.js
@@ -2,7 +2,7 @@ Video = Backbone.Model.extend({
     defaults: {
     	id : "Unassigned",
 		title : "Unassigned",
-		description : "Unassigned"
+		description : "Unassigned",
 		videoMp4Url : "Unassigned",
 		videoOgvUrl : "Unassigned",
 		videoWebMUrl : "Unassigned",
@@ -13,10 +13,8 @@ Video = Backbone.Model.extend({
     initialize : function (){
     	this.set({"selector":"video[id=\""+this.get("id")+"\"]"});
     	this.set({ "fieldHtml": this.render() });
-    	this.reset();
     },
 	render : function(){
-		this.isValid();
 		var objTemplate = templateFactory.getTemplate( this.get("template") );
 		return( objTemplate.render( this.toJSON() ) );
 	}
