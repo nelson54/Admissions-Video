16a1002d52a2fbd7efaf8ce5eb9fc5cec6e4c4d6
Derek Nelson <dereknelson01@gmail.com>
2011-08-16 14:49:51 -0400

    HTML5 video fixes.

--- video.js
+++ video.js
@@ -4,8 +4,11 @@ Video = Backbone.Model.extend({
 		title : "Unassigned",
 		description : "Unassigned",
 		videoMp4Url : "Unassigned",
+		videoMp4Details : 'type\'video\/mp4; codecs="avc1.42E01E, mp4a.40.2"\'',
 		videoOgvUrl : "Unassigned",
+		videoOgvDetails : 'type=\'video/webm; codecs="vp8, vorbis"\'',
 		videoWebMUrl : "Unassigned",
+		videoWebMDetails : 'type=\'video/ogg; codecs="theora, vorbis"\'',
 		thumbnailSmallUrl : "Unassigned",
 		thumbnailLargeUrl : "Unassigned",
 		template : "videoTemplate"
